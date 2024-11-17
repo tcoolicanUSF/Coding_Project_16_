0c7401d458fe2774c9b2ebdc2872b4f80cbb82af
tcoolican <tcoolican@usf.edu>
2024-11-17 18:56:53 -0500

    Set up App component with initial product list

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import ProductList from './ProductList';
+import AddProductForm from './AddProductForm';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [products] = useState([
+    {
+      id: 1,
+      name: 'Riddell Axiom',
+      price: 750.00,
+      description: 'A clearer vison, A truer fit, A smarter helmet.',
+    },
+    {
+      id: 2,
+      name: 'Riddell Speedflex',
+      price: 499.99,
+      description: 'State-of-the-art protection. Backed by extensive research and data from our millions of on-field impacts.',
+    },
+    {
+      id: 3,
+      name: 'Schutt F7',
+      price: 499.99,
+      description: 'Not only does the Schutt F7 Collegiate Football Helmet excel in safety, but it also prioritizes player comfort.',
+    },
+  ]);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <div>
+      <h1>Helmet Description App</h1>
+      <ProductList products={products} />
+      <AddProductForm />
+      <h3>Hello</h3>
+    </div>
+  );
+};
+export default App;
